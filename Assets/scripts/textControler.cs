0a6be9d9dffa2f2febd3996cf4b3d39f15f65ccd
paugal <pau.galan22@gmail.com>
2021-06-04 14:06:56 +0200

    Text update

---
+++ Assets/scripts/textControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class textControler : MonoBehaviour
+{
+    public TextMeshPro Start_Text_1;
+    public GameObject helicopter_1;
+    public GameObject helicopter_2;
+    public GameObject police_1;
+    public GameObject police_2;
+    public Light auxLight;
+
+    public TextMeshPro Start_Text_2;
+    public TextMeshPro Win_Text_1;
+    public TextMeshPro Win_Text_2;
+    public TextMeshPro Game_Over_Text_1;
+    public TextMeshPro Game_Over_Text_2;
+    public TextMeshPro Counter_Text_1;
+    public TextMeshPro Counter_Text_2;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void win(){}
+
+    void gameOver(){}
+
+    void counter(){}
+}

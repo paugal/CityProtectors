92df1ca603dfc82693512f8cc6aa4b53379ffe47
paugal <pau.galan22@gmail.com>
2021-06-09 15:14:08 +0200

    Enemigo desaparece cuando esta lejos, añadido modelo al enemigo y tamaño del helicoptero reducido

---
+++ Assets/scripts/controlRenderEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlRenderEnemy : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public GameObject player1;
+    public GameObject player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distPlayer1 = Vector3.Distance(enemy.transform.position, player1.transform.position);
+        float distPlayer2 = Vector3.Distance(enemy.transform.position, player2.transform.position);
+        MeshRenderer render = gameObject.GetComponent<MeshRenderer>();
+        if(distPlayer1 < 15 || distPlayer2 < 35){
+            render.enabled = false;
+        }else{
+            render.enabled = true;
+        }
+        
+        
+
+    }
+}

37f4f20173c1225856c007098bb9d9dca036c4a1
paugal <pau.galan22@gmail.com>
2021-06-13 17:02:34 +0200

    fixes

--- Assets/scripts/helicopterController.cs
+++ Assets/scripts/helicopterController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class controlHeliPointer : MonoBehaviour
+public class helicopterController : MonoBehaviour
 {
 
     public float rayLength = 1f;
